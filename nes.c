210fca64126555e02a827e9a441b01d7b1aabd6d
MetaDark <kurt.bruneau@gmail.com>
2015-10-20 13:51:24 -0400

    Increase PC on user input

--- nes.c
+++ nes.c
@@ -1,14 +1,18 @@
-#include "nes.h"
+#include <stdio.h>
 
+#include "nes.h"
 #include "cartridge.h"
 #include "memory.h"
 
 NES * nes_new(GFile * rom_file) {
   NES * nes = g_malloc(sizeof(NES));
-  nes->cpu = cpu_new(memory_new(cartridge_new(rom_file), 2048));
 
-  int i;
-  for (i = 0; i < 1; ++i) {
+  Cartridge * cartridge = cartridge_new(rom_file);
+  Memory * memory = memory_new(cartridge, 2048);
+  nes->cpu = cpu_new(memory);
+
+  char buffer[256];
+  while(fgets(buffer, ARRAY_LENGTH(buffer), stdin) != NULL) {
     cpu_eval_next(nes->cpu);
   }
   return nes;
