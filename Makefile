53951924353bb8dcf65070ec33f3cdb681d91f2f
MetaDark <kurt.bruneau@gmail.com>
2015-10-20 08:16:00 -0400

    Added NES loader

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-EXTERNAL_LIBS=glfw3 gl
+EXTERNAL_LIBS=glfw3 gl glib-2.0 gio-2.0
 
 CFLAGS += `pkg-config --cflags $(EXTERNAL_LIBS)`
 LDFLAGS += `pkg-config --libs $(EXTERNAL_LIBS)`
@@ -6,5 +6,8 @@ LDFLAGS += `pkg-config --libs $(EXTERNAL_LIBS)`
 main: main.c render.c events.c
 	$(CC) -o $@ $(CFLAGS) $^ $(LDFLAGS)
 
+loader: loader.c
+	$(CC) -o $@ $(CFLAGS) $^ $(LDFLAGS)
+
 clean:
-	rm -rf main
+	rm -rf main loader
---
+++ loader.c
@@ -0,0 +1,143 @@
+#include <glib.h>
+#include <gio/gio.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+/**
+ * Find ROM files recursively in a directory
+ */
+static GList * find_roms(GList * rom_list, const char * dir_name) {
+  GDir * dir = g_dir_open(dir_name, 0, NULL);
+  if (!dir) {
+    return rom_list;
+  }
+
+  const gchar * file_name = g_dir_read_name(dir);
+  while (file_name != NULL) {
+    gchar * file_path = g_build_filename(dir_name, file_name, NULL);
+
+    if (g_file_test(file_path, G_FILE_TEST_IS_REGULAR))  {
+      rom_list = g_list_append(rom_list, file_path);
+    } else if (g_file_test(file_path, G_FILE_TEST_IS_DIR)) {
+      find_roms(rom_list, file_path);
+      g_free(file_path);
+    } else {
+      g_free(file_path);
+    }
+
+    file_name = g_dir_read_name(dir);
+  }
+
+  g_dir_close(dir);
+  return rom_list;
+}
+
+/**
+ * List a single ROM item for selection
+ */
+static void list_rom(gpointer rom_name, gpointer user_data) {
+  int * index = user_data;
+  printf("%i) Play \"%s\"\n", *index, rom_name);
+  *index += 1;
+}
+
+/*
+ * Prompt the user to select a ROM from a list
+ */
+static GFile * select_rom(GList * rom_list) {
+  const gchar * file_name;
+
+  do {
+    int index = 0;
+    printf("%i) Quit\n", index++);
+    g_list_foreach(rom_list, list_rom, &index);
+    printf("Select action: ");
+
+    int input;
+    if (scanf("%d", &input) != 1) {
+      // This just does an infinite loop for some reason
+      continue;
+    }
+
+    if (input == 0) {
+      return NULL;
+    }
+
+    file_name = g_list_nth_data(rom_list, input - 1);
+    if (!file_name) {
+      printf("%s\n", "Invalid!");
+      continue;
+    }
+
+    break;
+  } while (1);
+
+  return g_file_new_for_path(file_name);
+}
+
+/*
+ * Parse a NES ROM file
+ */
+static void read_rom(GFile * rom_file) {
+  GInputStream * stream = (GInputStream *)g_file_read(rom_file, NULL, NULL);
+
+  // Read header
+  struct NESHeader {
+    unsigned char magic[4];
+    unsigned char prg_rom_size;
+    unsigned char chr_rom_size;
+
+    // Flags 6
+    unsigned char mirroring        : 1;
+    unsigned char prg_ram          : 1;
+    unsigned char trainer          : 1;
+    unsigned char four_screen_vram : 1;
+    unsigned char mapper_lower     : 4;
+
+    // Flags 7
+    unsigned char vs_unisystem : 1;
+    unsigned char playchoice10 : 1;
+    unsigned char version      : 2;
+    unsigned char mapper_upper : 4;
+
+    unsigned char prg_ram_size;
+    unsigned char zero[7];
+  } header;
+
+  g_input_stream_read(stream, &header, sizeof(header), NULL, NULL);
+
+  // Read trainer (don't worry about this yet)
+  if (header.trainer) {
+    g_input_stream_skip(stream, 512, NULL, NULL);
+  }
+
+  // Read PRG ROM data
+  size_t prg_rom_size = 16384 * header.prg_rom_size;
+  int * prg_rom = g_malloc(prg_rom_size);
+  g_input_stream_read(stream, prg_rom, prg_rom_size, NULL, NULL);
+  g_free(prg_rom);
+
+  // Read CHR ROM data
+  size_t chr_rom_size = 8192 * header.chr_rom_size;
+  void * chr_rom = g_malloc(chr_rom_size);
+  g_input_stream_read(stream, &chr_rom, chr_rom_size, NULL, NULL);
+  g_free(chr_rom);
+}
+
+int main() {
+  GList * rom_list = NULL;
+  rom_list = find_roms(rom_list, "roms/");
+  rom_list = g_list_sort(rom_list, (GCompareFunc)strcmp);
+
+  GFile * rom_file = select_rom(rom_list);
+  g_list_free_full(rom_list, g_free);
+  if (!rom_file) {
+    return 0;
+  }
+
+  read_rom(rom_file);
+  g_object_unref(rom_file);
+  return 0;
+}
