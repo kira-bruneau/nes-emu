27b9137540730d890cac7b960f2cffe8600ed2c8
MetaDark <kurt.bruneau@gmail.com>
2015-11-02 13:04:23 -0500

    Added frequency scaler

--- src/ui/render.c
+++ src/ui/render.c
@@ -5,7 +5,8 @@
 #include "ui/audio.h"
 #include "apu/apu.h"
 
-#define CPU_FREQUENCY 1789773
+#define CPU_FREQUENCY 39375000 / 22.0f // ~ 1789773 Hz
+#define FRAME_RATE 60
 
 GLFWwindow * window;
 
@@ -15,6 +16,7 @@ Audio * audio;
 double last_second;
 int cycle_count;
 int sample_count;
+int render_clock;
 
 void render_init(GLFWwindow * w) {
   window = w;
@@ -24,28 +26,48 @@ void render_init(GLFWwindow * w) {
   audio_start(audio);
 
   last_second = 0.0f;
+  render_clock = 0;
+}
+
+static int frequency_scale(float ratio, int clock) {
+  int int_part = (int)ratio;
+  float frac_part = ratio - int_part;
+
+  int result = int_part;
+  if ((int)(clock * frac_part) != (int)((clock + 1) * frac_part)) {
+    result += 1;
+  }
+
+  return result;
 }
 
 static void render_audio() {
+  // Assume render loop is clocked at 60Hz (for now)
   double now = glfwGetTime();
 
   if (now - 1.0f >= last_second) {
-    printf("%f: %i, %i\n", now, cycle_count, sample_count);
+    printf("%f (%i): %i, %i\n", now, render_clock, cycle_count, sample_count);
     cycle_count = sample_count = 0;
     last_second = now;
   }
 
-  int cpu_steps = 29830; /* 29829.54545454545454545455 */
-  while (cpu_steps-- > 0) {
+  int num_cycles = frequency_scale(CPU_FREQUENCY / FRAME_RATE, render_clock);
+  while (num_cycles-- != 0) {
     cycle_count += 1;
   }
 
-  int sample_steps = 735;
-  while (sample_steps-- > 0) {
+  int num_samples = SAMPLE_RATE / FRAME_RATE;
+  if (render_clock % (FRAME_RATE / 20) == 0) {
+    ++num_samples;
+  }
+
+  while (num_samples-- != 0) {
     apu_tick(&apu);
     audio_write(audio, apu_sample(&apu));
     sample_count += 1;
   }
+
+  render_clock += 1;
 }
 
 static void render_video() {
